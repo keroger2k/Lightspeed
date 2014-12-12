a1006dbadf9574d675bfeb0ad94e8861ea24d7d2
Kyle Rogers <kyle.rogers@gmail.com>
2014-12-11 21:04:18 -0600

    fixed dynamic issue.  need major organization.

--- Lightspeed/Program.cs
+++ Lightspeed/Program.cs
@@ -1,15 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Net;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
-using System.Web;
 using System.Web.Script.Serialization;
-using System.Dynamic;
 using System.Configuration;
 
 namespace Lightspeed
@@ -35,7 +30,8 @@ namespace Lightspeed
         {
             string API_KEY = ConfigurationManager.AppSettings["API_KEY"];
             string API_URL =
-                       string.Format("https://api.merchantos.com/API/Account/{0}/Item/{1}.json?callback=", ConfigurationManager.AppSettings["CLIENT_ID"], itemId);
+                       string.Format("https://api.merchantos.com/API/Account/{0}/Item/{1}.json?callback=", 
+                       ConfigurationManager.AppSettings["CLIENT_ID"], itemId);
             var client = new HttpClient();
             client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", Convert.ToBase64String(
             ASCIIEncoding.ASCII.GetBytes(string.Format("{0}:{1}",
@@ -46,8 +42,7 @@ namespace Lightspeed
             StreamReader reader = new StreamReader(stream);
             JavaScriptSerializer js = new JavaScriptSerializer();
             var tmpNestObject = js.Deserialize<dynamic>(reader.ReadToEnd());
-            var z = tmpNestObject.TryGetIndex(1);
-            Item obj = js.Deserialize<Item>(js.Serialize(tmpNestObject["item"]));
+            Item obj = js.Deserialize<Item>(js.Serialize(tmpNestObject["Item"]));
             reader.Close();
             return obj;
         }
@@ -56,7 +51,7 @@ namespace Lightspeed
         public class Item
         {
             public int itemId { get; set; }
-            public int systemSku { get; set; }
+            public double systemSku { get; set; }
             public decimal defaultCost { get; set; }
             public string description { get; set; }
         }
